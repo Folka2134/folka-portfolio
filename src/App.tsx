293607ac612259a15513022f6ae59b7fed216f87
Folka2134 <folka2134@gmail.com>
2023-12-05 15:51:24 +0000

    created about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import About from "./components/About";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="min-h-screen min-w-[450px] bg-black">
-      <Header />
-      <main></main>
+    <div className="h-full min-h-screen min-w-[450px] bg-black">
+      <div className="mx-10 pt-5 xl:pt-0">
+        <Header />
+        <main className="my-5 h-full rounded-2xl bg-purple-700 xl:my-12">
+          <About />
+        </main>
+        <p>test</p>
+      </div>
     </div>
   );
 }
