43e92b62fb23ffc72eec71a775b0201e3a206691
Folka2134 <folka2134@gmail.com>
2023-12-05 15:52:13 +0000

    updated html tags

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,13 @@
 const About = () => {
   return (
-    <div className="rounded-2xl bg-[#F1A277] p-4 md:w-1/2">
+    <section className="rounded-2xl bg-[#F1A277] p-4 md:w-1/2">
       <h2 className="p-3 text-3xl font-semibold">Hey!</h2>
       <p className="text-md lg:text-lg">
         I'm Otis, AKA Folka. A fresh software developer from Brighton, UK.{" "}
         <br />
         Currently on the hunt coding oppotunities that will expand my knowledge.
       </p>
-    </div>
+    </section>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <section className="bg-header-image flex h-52 items-center justify-center rounded-2xl bg-contain md:h-96 xl:min-h-screen xl:bg-cover xl:bg-no-repeat">
+    <div className="bg-header-image flex h-52 items-center justify-center rounded-2xl bg-contain md:h-96 xl:min-h-screen xl:bg-cover xl:bg-no-repeat">
       <h1 className="font-header text-4xl">Folka.Dev</h1>
-    </section>
+    </div>
   );
 };
 
