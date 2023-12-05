3cb0ff14e7ac95874b0471d20e0c60b45a452472
Folka2134 <folka2134@gmail.com>
2023-12-05 17:39:04 +0000

    updated styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="bg-header-image flex h-52 items-center justify-center rounded-2xl bg-contain md:h-96 xl:min-h-screen xl:bg-cover xl:bg-no-repeat">
+    <div className="flex h-52 items-center justify-center rounded-2xl bg-[url(src/assets/images/header-background.jpg)] bg-contain opacity-70 md:h-96 xl:min-h-screen xl:bg-cover xl:bg-no-repeat">
       <h1 className="font-header text-4xl">Folka.Dev</h1>
     </div>
   );
