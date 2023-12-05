07624507eab872fda672af036acd1edacb5f0118
Folka2134 <folka2134@gmail.com>
2023-12-05 17:38:14 +0000

    added particles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,88 @@
+import { useCallback } from "react";
+import Particles from "react-particles";
+import type { Container, Engine } from "tsparticles-engine";
+import { loadSlim } from "tsparticles-slim";
 import "./App.css";
 import About from "./components/About";
 import Header from "./components/Header";
+import Banner from "./components/Banner";
 
 function App() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    [],
+  );
+
+  const particleOptions = {
+    background: {
+      color: {
+        value: "#000000",
+      },
+    },
+    fpsLimit: 120,
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 70,
+        enable: true,
+        opacity: 0.5,
+        width: 0.5,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: {
+          default: "bounce",
+        },
+        random: false,
+        speed: 2,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 700,
+        },
+        value: 10,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: { min: 0.2, max: 5 },
+      },
+    },
+    detectRetina: true,
+  };
+
   return (
-    <div className="h-full min-h-screen min-w-[450px] bg-black">
-      <div className="mx-10 pt-5 xl:pt-0">
+    <div className="relative h-full min-h-screen min-w-[450px] bg-black">
+      <Particles
+        className="absolute left-0 top-0 z-10"
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={particleOptions}
+      />
+
+      <div className="relative z-20 mx-10 pt-5 xl:pt-0">
         <Header />
-        <main className="my-5 h-full rounded-2xl bg-purple-700 xl:my-12">
+        <main className="my-5 h-full rounded-2xl  xl:my-12">
           <About />
+          <Banner />
         </main>
-        <p>test</p>
       </div>
     </div>
   );
