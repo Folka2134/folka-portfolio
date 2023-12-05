68ee8fbcee6b8449b6e7a4d68c4e0c7a064be5e2
Folka2134 <folka2134@gmail.com>
2023-12-05 15:13:04 +0000

    created header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <div className="min-h-screen min-w-[450px] bg-black">
+      <Header />
+      <main></main>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="bg-header-image mx-10 flex h-52 items-center justify-center rounded-2xl bg-contain md:h-96 xl:min-h-screen xl:bg-cover xl:bg-no-repeat">
+      <h1 className="font-header text-4xl">Folka.Dev</h1>
+    </div>
+  );
+};
+
+export default Header;
