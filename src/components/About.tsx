309492d673cb38cd034de4ff40900e5b29b26d19
Folka2134 <folka2134@gmail.com>
2023-12-05 17:38:24 +0000

    created about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,12 @@
 const About = () => {
   return (
-    <section className="rounded-2xl bg-[#F1A277] p-4 md:w-1/2">
-      <h2 className="p-3 text-3xl font-semibold">Hey!</h2>
-      <p className="text-md lg:text-lg">
+    <section className="rounded-2xl bg-[#F1A277] bg-opacity-80 p-8 md:w-2/3 lg:w-1/2 xl:w-1/3 xl:p-12">
+      <h2 className="p-3 text-2xl font-semibold xl:text-4xl">Hey!</h2>
+      <p className="text-md text-[#212832] xl:text-2xl">
         I'm Otis, AKA Folka. A fresh software developer from Brighton, UK.{" "}
         <br />
-        Currently on the hunt coding oppotunities that will expand my knowledge.
+        Currently on the hunt for coding oppotunities that will expand my
+        knowledge.
       </p>
     </section>
   );
