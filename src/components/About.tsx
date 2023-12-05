ae574706e1804a48253964b674ea7e97fb373077
Folka2134 <folka2134@gmail.com>
2023-12-05 18:00:24 +0000

    updated styles

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <section className="rounded-2xl bg-[#F1A277] bg-opacity-80 p-8 md:w-2/3 lg:w-1/2 xl:w-1/3 xl:p-12">
+    <section className="rounded-2xl border-[1px] border-[#F1A277] bg-[#F1A277] bg-opacity-80 p-8 md:w-2/3 lg:w-1/2 xl:w-1/3 xl:p-12">
       <h2 className="p-3 text-2xl font-semibold xl:text-4xl">Hey!</h2>
       <p className="text-md text-[#212832] xl:text-2xl">
         I'm Otis, AKA Folka. A fresh software developer from Brighton, UK.{" "}
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -20,57 +20,57 @@ const Banner = () => {
           className: "opacity-80 transition-all duration-500",
         }}
       >
-        <div className="mt-2 overflow-hidden rounded-2xl bg-[#011B32] bg-opacity-90 p-10 ">
+        <div className="mt-4 overflow-hidden rounded-2xl border-[1px] border-[#0068B1] bg-[#0068B1] bg-opacity-25 p-10 ">
           <div className="flex w-full flex-wrap justify-around text-2xl md:text-3xl">
-            <div className="group text-center">
-              <FaHtml5 className="group-hover:text-[#e94546]" />
+            <div className="group p-5 text-center">
+              <FaHtml5 className="hover:text-[#e94546]" />
               <span className="transition-all duration-500 group-hover:text-white">
                 HTML
               </span>
             </div>
-            <div className="group text-center">
-              <FaCss3Alt className="hover:text-blue-600" />{" "}
+            <div className="group p-5 text-center">
+              <FaCss3Alt className="hover:text-blue-600" />
               <span className="transition-all duration-500 group-hover:text-white">
                 CSS
               </span>
             </div>
-            <div className="group text-center">
+            <div className="group p-5 text-center">
               <FaJsSquare className="hover:text-[#f7df1e]" />
               <span className="transition-all duration-500 group-hover:text-white">
                 JavaScript
               </span>
             </div>
-            <div className="group text-center">
-              <FaReact className="hover:text-blue-400" />{" "}
+            <div className="group p-5 text-center">
+              <FaReact className="hover:text-blue-400" />
               <span className="transition-all duration-500 group-hover:text-white">
                 React
               </span>
             </div>
-            <div className="group text-center">
+            <div className="group p-5 text-center">
               <FaNodeJs className="hover:text-[#83cd29]" />
               <span className="transition-all duration-500 group-hover:text-white">
                 NodeJs
               </span>
             </div>
-            <div className="group text-center">
+            <div className="group p-5 text-center">
               <SiExpress className="hover:text-red-700" />
               <span className="transition-all duration-500 group-hover:text-white">
                 Express
               </span>
             </div>
-            <div className="group text-center">
+            <div className="group p-5 text-center">
               <FaPython className="hover:text-[#316192]" />
               <span className="transition-all duration-500 group-hover:text-white">
                 Python
               </span>
             </div>
-            <div className="group text-center">
+            <div className="group p-5 text-center">
               <SiPostgresql className="hover:text-[#316192]" />
               <span className="transition-all duration-500 group-hover:text-white">
                 Prostgres
               </span>
             </div>
-            <div className="group text-center">
+            <div className="group p-5 text-center">
               <SiMongodb className="hover:text-[#4c8e42]" />
               <span className="transition-all duration-500 group-hover:text-white">
                 MongoDB
