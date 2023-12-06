21ce16081c13f6e42b2405802b5a4ed33ee4245a
Folka2134 <folka2134@gmail.com>
2023-12-06 17:28:55 +0000

    added animations

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,26 +1,55 @@
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
 import { Link } from "react-scroll";
 
 const About = () => {
+  const controls = useAnimation();
+
+  const handleScroll = () => {
+    // Get the vertical scroll position
+    const scrollY = window.scrollY;
+
+    // Define the trigger point (adjust as needed)
+    const triggerPoint = window.innerHeight * 0.1;
+
+    // Check if the element is in the viewport
+    if (scrollY > triggerPoint) {
+      controls.start({ x: 0, opacity: 1 });
+    }
+  };
+
+  // Attach the scroll event listener on mount
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <section
-      id="about"
-      className="rounded-2xl border-[1px] border-[#F1A277] bg-white bg-opacity-80 p-8 md:w-2/3 lg:w-1/2 xl:w-1/3 xl:p-12"
+    <motion.div
+      initial={{ x: "-200%", opacity: 0 }}
+      animate={controls}
+      transition={{ duration: 0.8 }}
     >
-      <h2 className="p-3 text-2xl font-semibold xl:text-4xl">Hey!</h2>
-      <p className="text-md text-[#212832] xl:text-2xl">
-        I'm Otis, AKA Folka. A fresh software developer from Brighton, UK.{" "}
-        <br />
-        Currently on the hunt for coding oppotunities to expand my knowledge.
-        <br />
-        <br />
-        Check out my{" "}
-        <Link to="projects-container" smooth={true}>
-          <span className="cursor-pointer font-semibold text-[#A32F31] underline underline-offset-4 transition-all hover:text-[#0090A5]">
-            Projects
-          </span>
-        </Link>
-      </p>
-    </section>
+      <section
+        id="about"
+        className="rounded-2xl border-[1px] border-[#F1A277] bg-white bg-opacity-80 p-8 md:w-2/3 lg:w-1/2 xl:w-1/3 xl:p-12"
+      >
+        <h2 className="p-3 text-2xl font-semibold xl:text-4xl">Hey!</h2>
+        <p className="text-md text-[#212832] xl:text-2xl">
+          I'm Otis, AKA Folka. A fresh software developer from Brighton, UK.{" "}
+          <br />
+          Currently on the hunt for coding oppotunities to expand my knowledge.
+          <br />
+          <br />
+          Check out my{" "}
+          <Link to="projects-container" smooth={true}>
+            <span className="cursor-pointer font-semibold text-[#A32F31] underline underline-offset-4 transition-all hover:text-[#0090A5]">
+              Projects
+            </span>
+          </Link>
+        </p>
+      </section>
+    </motion.div>
   );
 };
 
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,5 @@
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
 import { IconContext } from "react-icons";
 
 import {
@@ -13,17 +15,43 @@ import {
 import { SiPostgresql, SiExpress, SiMongodb } from "react-icons/si";
 
 const Banner = () => {
+  const controls = useAnimation();
+
+  const handleScroll = () => {
+    // Get the vertical scroll position
+    const scrollY = window.scrollY;
+
+    // Define the trigger point (adjust as needed)
+    const triggerPoint = window.innerHeight * 0.1;
+
+    // Check if the element is in the viewport
+    if (scrollY > triggerPoint) {
+      controls.start({ y: 0, opacity: 1 });
+    }
+  };
+
+  // Attach the scroll event listener on mount
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div>
-      <IconContext.Provider
-        value={{
-          size: "155",
-          className: "opacity-80 transition-all duration-500",
-        }}
-      >
-        <div className="mt-4 overflow-hidden rounded-2xl bg-[#0068B1] bg-opacity-25 p-10 ">
-          <div className="flex w-full flex-wrap justify-center text-2xl md:text-3xl">
-            {/* <div className="group p-5 text-center">
+    <motion.div
+      initial={{ y: "200%", opacity: 0 }}
+      animate={controls}
+      transition={{ duration: 1.2, type: "spring", stiffness: 70 }}
+    >
+      <div>
+        <IconContext.Provider
+          value={{
+            size: "155",
+            className: "opacity-80 transition-all duration-500",
+          }}
+        >
+          <div className="mt-4 overflow-hidden rounded-2xl bg-[#0068B1] bg-opacity-25 p-10 ">
+            <div className="flex w-full flex-wrap justify-center text-2xl md:text-3xl">
+              {/* <div className="group p-5 text-center">
               <FaHtml5 className="hover:text-[#e94546]" />
               <span className="transition-all duration-500 group-hover:text-white">
                 HTML
@@ -35,58 +63,59 @@ const Banner = () => {
                 CSS
               </span>
             </div> */}
-            <div className="group p-5 text-center">
-              <FaJsSquare className="hover:text-[#f7df1e]" />
-              <span className="transition-all duration-500 group-hover:text-white">
-                JavaScript
-              </span>
-            </div>
-            <div className="group p-5 text-center">
-              <FaReact className="hover:text-blue-400" />
-              <span className="transition-all duration-500 group-hover:text-white">
-                React
-              </span>
-            </div>
-            <div className="group p-5 text-center">
-              <FaNodeJs className="hover:text-[#83cd29]" />
-              <span className="transition-all duration-500 group-hover:text-white">
-                NodeJs
-              </span>
-            </div>
-            <div className="group p-5 text-center">
-              <SiExpress className="hover:text-red-700" />
-              <span className="transition-all duration-500 group-hover:text-white">
-                Express
-              </span>
-            </div>
-            <div className="group p-5 text-center">
-              <FaPython className="hover:text-[#316192]" />
-              <span className="transition-all duration-500 group-hover:text-white">
-                Python
-              </span>
-            </div>
-            <div className="group p-5 text-center">
-              <SiPostgresql className="hover:text-[#316192]" />
-              <span className="transition-all duration-500 group-hover:text-white">
-                Prostgres
-              </span>
-            </div>
-            <div className="group p-5 text-center">
-              <SiMongodb className="hover:text-[#4c8e42]" />
-              <span className="transition-all duration-500 group-hover:text-white">
-                MongoDB
-              </span>
-            </div>
-            <div className="group group p-5 text-center">
-              <FaGitAlt className="hover:text-[#f05033]" />
-              <span className="transition-all duration-500 group-hover:text-white">
-                Git
-              </span>
+              <div className="group p-5 text-center">
+                <FaJsSquare className="hover:text-[#f7df1e]" />
+                <span className="transition-all duration-500 group-hover:text-white">
+                  JavaScript
+                </span>
+              </div>
+              <div className="group p-5 text-center">
+                <FaReact className="hover:text-blue-400" />
+                <span className="transition-all duration-500 group-hover:text-white">
+                  React
+                </span>
+              </div>
+              <div className="group p-5 text-center">
+                <FaNodeJs className="hover:text-[#83cd29]" />
+                <span className="transition-all duration-500 group-hover:text-white">
+                  NodeJs
+                </span>
+              </div>
+              <div className="group p-5 text-center">
+                <SiExpress className="hover:text-red-700" />
+                <span className="transition-all duration-500 group-hover:text-white">
+                  Express
+                </span>
+              </div>
+              <div className="group p-5 text-center">
+                <FaPython className="hover:text-[#316192]" />
+                <span className="transition-all duration-500 group-hover:text-white">
+                  Python
+                </span>
+              </div>
+              <div className="group p-5 text-center">
+                <SiPostgresql className="hover:text-[#316192]" />
+                <span className="transition-all duration-500 group-hover:text-white">
+                  Prostgres
+                </span>
+              </div>
+              <div className="group p-5 text-center">
+                <SiMongodb className="hover:text-[#4c8e42]" />
+                <span className="transition-all duration-500 group-hover:text-white">
+                  MongoDB
+                </span>
+              </div>
+              <div className="group group p-5 text-center">
+                <FaGitAlt className="hover:text-[#f05033]" />
+                <span className="transition-all duration-500 group-hover:text-white">
+                  Git
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-      </IconContext.Provider>
-    </div>
+        </IconContext.Provider>
+      </div>
+    </motion.div>
   );
 };
 
