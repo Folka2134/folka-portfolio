9e9ba4c766956019f9dc3e8b5f342b72b0ed99a8
Folka2134 <folka2134@gmail.com>
2023-12-06 14:26:37 +0000

    updated text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,8 +5,7 @@ const About = () => {
       <p className="text-md text-[#212832] xl:text-2xl">
         I'm Otis, AKA Folka. A fresh software developer from Brighton, UK.{" "}
         <br />
-        Currently on the hunt for coding oppotunities that will expand my
-        knowledge.
+        Currently on the hunt for coding oppotunities to expand my knowledge.
       </p>
     </section>
   );
