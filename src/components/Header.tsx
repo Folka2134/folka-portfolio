20974f2ea5fb6d73cec24140d65d23d38b7e4fb4
Folka2134 <folka2134@gmail.com>
2023-12-06 17:03:12 +0000

    updated styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="flex h-52 items-center justify-center rounded-2xl bg-[url(src/assets/images/header-background.jpg)] bg-contain opacity-70 md:h-96 xl:min-h-screen xl:bg-cover xl:bg-no-repeat">
       <Link to="about-container" smooth={true}>
-        <h1 className="font-header px-3 text-7xl text-[#A32F31] outline-dashed outline-2 outline-offset-2">
+        <h1 className="font-header duration-5 animate-pulse cursor-pointer px-3 text-4xl text-[#A32F31] outline-dashed outline-2 outline-offset-2 md:text-7xl">
           folka.dev
         </h1>
       </Link>
