db6ae82fe5594c40efecf536b52bcc684513d81c
Folka2134 <folka2134@gmail.com>
2023-12-06 14:26:59 +0000

    created projects page

---
+++ src/components/Projects.tsx
@@ -0,0 +1,9 @@
+const Projects = () => {
+  return (
+    <div className="mt-14 flex min-h-screen items-center justify-center rounded-2xl bg-purple-900">
+      <h1>projects</h1>
+    </div>
+  );
+};
+
+export default Projects;
