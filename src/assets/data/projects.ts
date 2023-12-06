6bf205a90439605f1076baf7252fd32244ee6bbc
Folka2134 <folka2134@gmail.com>
2023-12-06 15:11:19 +0000

    added project data

---
+++ src/assets/data/projects.ts
@@ -0,0 +1,22 @@
+export const projectData = [
+  {
+    title: "Face Detection App",
+    image: "/src/assets/images/smartbrain_preview2.png",
+    description: "This app was created to 'detect' faces within an image and also update a signed in user's 'rank'.",
+    functionality: "The detection is performed by pulling data from an api and the users are stored within my own database.",
+    improvements: ["Implement multi-face detection", "Implement user score board feature", "Adjust design for mobile"],
+    tags: ["React", "Nodejs", "Expressjs", "Prostgresql"],
+    liveUrl: "https://folka2134.github.io/face-detection-app/",
+    githubUrl: "https://github.com/Folka2134/face-detection-app",
+  },
+  {
+    title: "Face Detection App",
+    image: "",
+    description: "This app was created to 'detect' faces within an image and also update a signed in user's 'rank'.",
+    functionality: "The detection is performed by pulling data from an api and the users are stored within my own database.",
+    improvements: ["Implement multi-face detection", "Implement user score board feature", "Adjust design for mobile"],
+    tags: ["React", "Nodejs", "Expressjs", "Prostgresql"],
+    liveUrl: "https://folka2134.github.io/face-detection-app/",
+    githubUrl: "https://github.com/Folka2134/face-detection-app",
+  }
+]
