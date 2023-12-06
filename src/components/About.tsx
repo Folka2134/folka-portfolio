55429b9964716f0e3edf4a2531779a7838486ff6
Folka2134 <folka2134@gmail.com>
2023-12-06 16:36:19 +0000

    added page links

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,24 @@
+import { Link } from "react-scroll";
+
 const About = () => {
   return (
-    <section className="rounded-2xl border-[1px] border-[#F1A277] bg-[#F1A277] bg-opacity-80 p-8 md:w-2/3 lg:w-1/2 xl:w-1/3 xl:p-12">
+    <section
+      id="about"
+      className="rounded-2xl border-[1px] border-[#F1A277] bg-white bg-opacity-80 p-8 md:w-2/3 lg:w-1/2 xl:w-1/3 xl:p-12"
+    >
       <h2 className="p-3 text-2xl font-semibold xl:text-4xl">Hey!</h2>
       <p className="text-md text-[#212832] xl:text-2xl">
         I'm Otis, AKA Folka. A fresh software developer from Brighton, UK.{" "}
         <br />
         Currently on the hunt for coding oppotunities to expand my knowledge.
+        <br />
+        <br />
+        Check out my{" "}
+        <Link to="projects-container" smooth={true}>
+          <span className="cursor-pointer font-semibold text-[#A32F31] underline underline-offset-4 transition-all hover:text-[#0090A5]">
+            Projects
+          </span>
+        </Link>
       </p>
     </section>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-scroll";
+
 const Header = () => {
   return (
     <div className="flex h-52 items-center justify-center rounded-2xl bg-[url(src/assets/images/header-background.jpg)] bg-contain opacity-70 md:h-96 xl:min-h-screen xl:bg-cover xl:bg-no-repeat">
-      <h1 className="font-header text-4xl">Folka.Dev</h1>
+      <Link to="about-container" smooth={true}>
+        <h1 className="font-header px-3 text-7xl text-[#A32F31] outline-dashed outline-2 outline-offset-2">
+          folka.dev
+        </h1>
+      </Link>
     </div>
   );
 };
