19885cba7f05cad2e188b486ee268ac8c541e84a
Folka2134 <folka2134@gmail.com>
2023-12-06 16:35:57 +0000

    updated styles

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,8 +2,11 @@ import { projectData } from "../assets/data/projects";
 
 const Projects = () => {
   return (
-    <div className="my-5 flex flex-col items-center justify-center rounded-2xl">
-      <h1>projects</h1>
+    <div
+      id="projects-container"
+      className="mb-5 flex flex-col items-center justify-center rounded-2xl pt-5 lg:mb-52"
+    >
+      <h1 className="my-2 h-16 text-2xl text-white">Projects</h1>
       <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
         {projectData.map((project) => (
           <div className="mx-5 mb-8 overflow-hidden rounded-2xl bg-white bg-opacity-95 shadow-md transition-all duration-300 ease-linear hover:shadow-2xl">
@@ -33,16 +36,18 @@ const Projects = () => {
             </div>
             <div className="flex justify-center gap-2">
               {project.tags.map((tag) => (
-                <span className="tag gap-2">{tag}</span>
+                <span className="tag gap-2 text-white hover:scale-110 ">
+                  {tag}
+                </span>
               ))}
             </div>
             <div className="flex justify-center gap-2 ">
-              <span className="button mb-2 mr-2 inline-block px-3 py-1 text-sm font-semibold">
+              <span className="button mb-2 mr-2 inline-block px-3 py-1 text-sm font-semibold hover:bg-[#0068B1] hover:text-white">
                 <a href={project.liveUrl} target="_blank" rel="noreferrer">
                   Live
                 </a>
               </span>
-              <span className="button mb-2 mr-2 inline-block px-3 py-1 text-sm font-semibold">
+              <span className="button mb-2 mr-2 inline-block px-3 py-1 text-sm font-semibold hover:bg-[#E94E31] hover:text-white">
                 <a href={project.githubUrl} target="_blank" rel="noreferrer">
                   Github
                 </a>
